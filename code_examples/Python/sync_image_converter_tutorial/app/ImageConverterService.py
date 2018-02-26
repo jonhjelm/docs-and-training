a2bb86df927440d0f21255ef84c86aa208bffc43
Robert Schittny <robert.schittny@sintef.no>
2018-02-26 12:35:53 +0100

    Strip down image-converter tutorial code

--- code_examples/Python/sync_image_converter_tutorial/app/ImageConverterService.py
+++ code_examples/Python/sync_image_converter_tutorial/app/ImageConverterService.py
@@ -1,9 +1,3 @@
-"""Simple synchronous service which converts png images to jpg images.
-
-Note that this service is mainly intended to showcase file access via GSS. The
-file access is coded explicitly here. In production code, one would rather wrap
-the GSS access into a library and use that.
-"""
 import os
 import shutil
 import tempfile
@@ -45,65 +39,11 @@ class ImageConverterService(spyne.Service):
     def imageconvert_png2jpg(sessionToken, extra_pars_str, gss_ID):
         """Converts a png image specified by a gss ID to a jpg image."""
 
-        # Get gss location URL from extra parameters
-        extra_pars = parse_extra_parameters(extra_pars_str)
-        gss_location = extra_pars['gss']
+        # Implementation starts here ...
+        gss_ID_new = 'fake!'
+        # ... and ends here
 
-        # obtain resource information for given GSS ID and check if read is
-        # allowed
-        res_info = get_resource_information(gss_location, gss_ID,
-                                            sessionToken)
-        read_desc = res_info.readDescription
-        if not read_desc.supported:
-            raise AttributeError('Read operation not allowed')
-
-        # Create a temporary folder to store the file in
-        tempdir = tempfile.mkdtemp()
-        gss_folder, png_filename = os.path.split(gss_ID)
-        png_filepath = os.path.join(tempdir, png_filename)
-
-        # Formulate RESTful request to download the file
-        # Therefore, we copy all headers defined in the requestDescription object
-        # into the request headers.
-        headers = {h.key: h.value for h in read_desc.headers}
-        request = urllib2.Request(url=read_desc.url, headers=headers)
-        with open(png_filepath, 'wb') as out_file:
-            result = urllib2.urlopen(request)
-            while True:
-                buffer = result.read()
-                if not buffer:
-                    break
-                out_file.write(buffer)
-
-        # Convert the file using imagemagick
-        jpg_filepath = convert_png2jpg(png_filepath)
-
-        # Create a new GSS ID to store the converted file in
-        _, jpg_filename = os.path.split(jpg_filepath)
-        gss_ID_new = os.path.join(gss_folder, jpg_filename)
-
-        # Make sure we are allowed to upload to this GSS ID.
-        # Room for improvement: if 'create' is not allowed (because the file
-        # already exists), use 'update' instead
-        res_info = get_resource_information(gss_location, gss_ID_new,
-                                            sessionToken)
-        create_desc = res_info.createDescription
-        if not create_desc.supported:
-            raise AttributeError('Create operation not allowed')
-
-        # Formulate RESTful request to upload the new file
-        # We again copy the headers from the requestDescription object
-        headers = {h.key: h.value for h in create_desc.headers}
-        headers["Content-Length"] = "%d" % os.stat(jpg_filepath).st_size
-
-        with open(jpg_filepath, "r") as in_file:
-            request = urllib2.Request(url=create_desc.url, data=in_file,
-                                      headers=headers)
-            request.get_method = lambda: create_desc.httpMethod
-            result = urllib2.urlopen(request)
-
-        # Remove temporary folder
-        shutil.rmtree(tempdir)
+        return gss_ID_new
 
 
 def get_resource_information(gss_location, gss_ID, session_token):
