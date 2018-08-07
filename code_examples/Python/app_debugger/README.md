c8f3ba353eb4295e1b588c75523c548f324ec42b
Robert Schittny <robert.schittny@sintef.no>
2018-08-07 09:00:32 +0200

    Add debugger application as a code example

---
+++ code_examples/Python/app_debugger/README.md
@@ -0,0 +1,101 @@
+Python-based CloudFlow application for workflow debugging
+=========================================================
+This CloudFlow application offers tools for debugging workflows.
+
+## `displayParameters` service
+Currently, only one application is available, registered under the following URI:
+```
+http://sintef/apps/displayParameters.owl#displayParameters_Service
+```
+This application pauses the workflow and offers to display any output parameters
+created so far.
+
+The following picture shows the service integrated in a bigger workflow. The
+`gss_transformationMatrix` output parameter of the previous workflow step is
+routed to one of the service's inputs, and a corresponding label is set.
+<p align="center">
+  <img src="doc/wfe.png"
+   alt="Minimal connections made to the file-chooser service" width="500px"/>
+</p>
+
+Upon execution, the service will pause the workflow and display a debug page
+similar to the following one:
+<p align="center">
+  <img src="doc/status.png"
+   alt="Minimal connections made to the file-chooser service" width="500px"/>
+</p>
+
+### Service parameters
+| Parameter name | Wiring required? | Description |
+| -------------- | --------- | ----------- |
+| `sessionToken` | yes | The session token used for authentication. Should be connected to the workflow input with the same name |
+| `serviceID` | no | Provided automatically by the WFM, must be left open. |
+| `extraParameters` | yes | Contains parameters such as the GSS location, must be wired to corresponding workflow input. |
+| `in1` - `in5` | no | Arbitrary parameter to be displayed |
+| `label1` - `label5`| no | Optional label for `in#` parameter, defaults to `"in#"` |
+
+## Development information
+### Prerequisites
+To build, run, and test this skeleton service, you only need to have Docker
+installed on your machine. All required software is already bundled in Docker
+containers.
+
+For further development based on this skeleton, it is highly recommended to
+use a local Python environment.
+
+### Configure, build, and run the service
+#### Configuration
+Prior to running the service, have a look at the file `env`. This file defines
+environment variables which will be passed into the container. The first one,
+`CONTEXT_ROOT`, defines the deployment path of the app relative to root.
+Essentially, this needs to be set to the relative path under which the VM
+hosting the service is reachable.
+
+You can define further configuration variables which then can be used in the
+application source code.
+
+#### Build and run
+To compile service source code, pack it into a Docker container, and run the
+container, run
+```
+./rebuildandrun.sh <port>
+```
+Choose a port number that is available on your machine or leave it away to
+default to port 8080.
+
+On the first run, this might take a while since the base container images need
+to be downloaded and dependencies need to be installed. Subsequent builds will
+complete significantly faster.
+
+This run script starts the container in daemon mode, meaning that the command
+returns immediately and that logs are not immediately visible.
+
+Alternatively, run the container interactively via:
+```
+docker run -p <port>:80 --env-file=env app-debugger
+```
+Again, choose a fitting port number.
+
+#### Testing the service
+Once the container is running, it can be tested by using its published wsdl
+file to create a SOAP webclient and call methods with it.
+
+This example includes a Python-based client application, found in the
+`test_client/` folder. If you have a local Python 3.x environment, you can
+simply run `python test_debugger.py` (make sure that all dependencies defined in
+`requirements.txt`) are installed. Alternatively, the example includes a Python
+Docker container for executing the test client.
+
+To use the Python container, run
+```
+cd test_client
+./build.sh      # run this only once
+./run.sh        # run this every time you want to test
+```
+
+The test client saves the application output to `test.html`, which you can 
+review with your browser.
+
+You can make changes to `test_debugger.py` to test other methods or other
+deployment locations. Rebuilding the container is not necessary after such
+changes.
---
+++ code_examples/Python/app_debugger/app/Debugger.py
@@ -0,0 +1,172 @@
+"""Application for debugging service outputs
+
+CloudFlow application which takes in a bunch of inputs and outputs them without
+any changes. Introduces a "breakpoint" in a workflow where all inputs are
+displayed.
+"""
+import os
+import subprocess
+import base64
+import datetime
+import logging
+
+from spyne import Application, srpc, ServiceBase, Unicode, Integer, Boolean
+from spyne.protocol.soap import Soap11
+
+# Define the target namespace
+TNS = "app-debugger.sintef.no"
+# Define the name under which the service will be deployed
+SERVICENAME = "Debugger"
+
+
+class DebuggerService(ServiceBase):
+    # Note that the class name does not influence the deployment endpoint
+    # under which the service will be reachable. It will, however, appear
+    # in the wsdl file.
+
+    @srpc(
+        Unicode, # serviceID
+        Unicode, # sessionToken
+        Unicode, # extraParameters
+        Unicode, # Input 1
+        Unicode, # Label 1
+        Unicode, # Input 2
+        Unicode, # Label 2
+        Unicode, # Input 3
+        Unicode, # Label 3
+        Unicode, # Input 4
+        Unicode, # Label 3
+        Unicode, # Input 5
+        Unicode, # Label 5
+        _returns=Unicode,
+        _out_variable_name="status_base64")
+    def displayParameters(serviceID, sessionToken, extraParameters,
+        in1="", label1="in1", 
+        in2="", label2="in2", 
+        in3="", label3="in3", 
+        in4="", label4="in4", 
+        in5="", label5="in5"):
+        """
+        Starts the debugger application.
+        """
+        logging.info("startDialog() called with service ID {}".format(serviceID))
+
+        eP_parsed = parse_extra_parameters(extraParameters)
+
+        html = HTML.format(
+            sid=serviceID,
+            stk=sessionToken,
+            wfm_endpoint=eP_parsed["WFM"],
+            eP=extraParameters,
+            in1=in1,
+            in2=in2,
+            in3=in3,
+            in4=in4,
+            in5=in5,
+            label1=label1,
+            label2=label2,
+            label3=label3,
+            label4=label4,
+            label5=label5,
+            result=RES_B64,
+        )
+
+        status = base64.b64encode(html.encode()).decode()
+
+        return status
+
+def create_app():
+    """Creates an Application object containing the debugger service."""
+    app = Application([DebuggerService], TNS,
+                      in_protocol=Soap11(validator='soft'), out_protocol=Soap11())
+
+    return app
+
+# HTML code which is delivered to the workflow manager. In this example, this
+# HTML code contains the complete application.
+HTML = """<html>
+<head>
+<script type="text/javascript">
+var request = new XMLHttpRequest();
+function cont_wf() {{
+    request.open("POST", "{wfm_endpoint}", false);
+    request.setRequestHeader("Content-Type", "text/xml");
+    request.setRequestHeader("SOAPAction", "");
+    var request_body = '<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:wor=\"http://www.eu-cloudflow.eu/dfki/WorkflowManager2/\">' +
+                       '  <soapenv:Header />' +
+                       '  <soapenv:Body>' +
+                       '    <wor:serviceExecutionFinished>' +
+                       '    <serviceID>{sid}</serviceID>' +
+                       '    <sessionToken>{stk}</sessionToken>' +
+                       '    <xmlOutputs_base64>{result}</xmlOutputs_base64>' +
+                       '    </wor:serviceExecutionFinished>' +
+                       '  </soapenv:Body>' +
+                       '</soapenv:Envelope>';
+	try {{
+        request.send(request_body);
+    }} catch (exception) {{
+        alert(exception);
+    }}
+}}
+</script>
+</head>
+<body>
+    <div style="padding: 30px;">
+    <h2>Variable debugger</h2>
+    <h3>General information</h3>
+    Service ID: {sid}</br>
+    Session Token: {stk}
+
+    <h3>Extra parameters</h3>
+    {eP}
+
+    <h3>Variable contents</h3>
+    <table>
+        <tr>
+            <th>Variable</th>
+            <th>Value</th>
+        </tr>
+        <tr>
+            <td>{label1}</td>
+            <td>{in1}</td>
+        </tr>
+        <tr>
+            <td>{label2}</td>
+            <td>{in2}</td>
+        </tr>
+        <tr>
+            <td>{label3}</td>
+            <td>{in3}</td>
+        </tr>
+        <tr>
+            <td>{label4}</td>
+            <td>{in4}</td>
+        </tr>
+        <tr>
+            <td>{label5}</td>
+            <td>{in5}</td>
+        </tr>
+    </table>
+    <input type="button" value="Click to leave debugger and continue workflow" onclick="cont_wf()">
+    </div>
+</body>
+</html>
+"""
+
+RES = """<ServiceOutputs>
+<status_base64>Finished</status_base64>
+</ServiceOutputs>
+"""
+RES_B64 = base64.b64encode(RES.encode()).decode()
+
+
+def parse_extra_parameters(extra_pars):
+    """Parses an extra-parameters string into a dict.
+
+    The extra parameters as delivered from the workflow manager are encoded in
+    a single string of the format "key1=value1,key2=value2,key3=value3,...".
+    Important: The string contains another comma at the very end.
+    """
+    print(extra_pars)
+    return {pair.split('=')[0]: pair.split('=')[1] for pair in
+            extra_pars.split(',')[:-1]}
