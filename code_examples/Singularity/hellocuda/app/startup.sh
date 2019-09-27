9f7eb0c0867d716140a760c89fdf68a70e85b85a
Jon Hjelmervik <jon.hjelmervik@gmail.com>
2019-09-27 15:53:24 +0200

    reports to result.txt also if nvidia-smi is unavailable

--- code_examples/Singularity/hellocuda/app/startup.sh
+++ code_examples/Singularity/hellocuda/app/startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo executing nvidia-smihellocuda > /service/status.html
 echo not done yet > /service/result.txt
-command -v nvidia-smi  >/dev/null 2>&1 && nvidia-smi 2>&1 | tee /service/result.txt || echo "failed to start nvidia-smi"
+(command -v nvidia-smi  >/dev/null 2>&1 && nvidia-smi || echo "failed to start nvidia-smi") 2>&1 | tee /service/result.txt
 echo executing nvidia-smihellocuda > /service/status.html
 /app/hellocuda  2>&1 | tee -a /service/result.txt
 echo done
