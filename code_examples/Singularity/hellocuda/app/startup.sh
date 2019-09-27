ee82e17e509dfac91bbc70ace07fd7bc00739a72
Jon Hjelmervik <jon.hjelmervik@gmail.com>
2019-09-27 14:42:44 +0200

    updated cuda sample to simplfy debugging

--- code_examples/Singularity/hellocuda/app/startup.sh
+++ code_examples/Singularity/hellocuda/app/startup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-echo executing hellocuda > /service/status.html
+echo executing nvidia-smihellocuda > /service/status.html
 echo not done yet > /service/result.txt
-/app/hellocuda  2>&1 | tee /service/result.txt
-nvidia-smi 2>&1 >> /service/result.txt
+command -v nvidia-smi  >/dev/null 2>&1 && nvidia-smi 2>&1 | tee /service/result.txt || echo "failed to start nvidia-smi"
+echo executing nvidia-smihellocuda > /service/status.html
+/app/hellocuda  2>&1 | tee -a /service/result.txt
 echo done
